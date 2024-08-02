1e7fb1f0964bd3a2e5b76ec0fa09a247ab796be1
acatejr <acatejr@gmail.com>
2024-08-02 08:42:51 -0700

    Refactoring rec_int_mult

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,32 @@
-/// # Examples:
-/// 
-/// ```
-/// let result = algorithms::rec_int_mult(1, 1);
-/// assert_eq!(result, 1);
-/// ```
-/// ```
-/// let result = algorithms::rec_int_mult(5, 5);
-/// assert_eq!(result, 25);
-/// ```
-/// ```
-/// let result = algorithms::rec_int_mult(25, 20);
-/// assert_eq!(result, 500);
-/// ```
+use std::cmp;
+
+/**
+# Examples:
+```
+let result = algorithms::rec_int_mult(5, 5);
+assert_eq!(result, 25);
+```
+```
+let result = algorithms::rec_int_mult(25, 20);
+assert_eq!(result, 500);
+```
+*/
 pub fn rec_int_mult(x: i32, y: i32) -> i32 {
     
-    let n: usize = x.to_string().len();
-    let x_str = String::from(x.to_string());
-    let y_str = String::from(y.to_string());
-    let mid = n/2;
-
+    let n = cmp::max(
+        (x as f32).log10().floor() as usize,
+        (y as f32).log10().floor() as usize,
+    ) + 1 as usize;
+    
     let prod: i32;
 
-    if n == 1 {
+    if n == 1 || x == 0 || y == 0{
         prod = x * y;
     } else {
+        let mid: usize = n as usize / 2;
+        let x_str = x.to_string();
+        let y_str = y.to_string();
+
         let a: i32 = x_str[0..mid].parse::<i32>().unwrap(); // first half of x
         let b: i32 = x_str[mid..n].parse::<i32>().unwrap(); // second half of x
 
@@ -35,10 +38,23 @@ pub fn rec_int_mult(x: i32, y: i32) -> i32 {
         let bc = rec_int_mult(b, c);
         let bd = rec_int_mult(b, d);
 
-        prod = i32::pow(10, n as u32) as i32 * ac + i32::pow(10, (n/2) as u32) * (ad + bc) + bd;
+        prod = i32::pow(10, n as u32) as i32 * ac + i32::pow(10, (n / 2) as u32) * (ad + bc) + bd;
     }
 
-
     return prod;
-    
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rec_int_mult_large_int_x_1000_y_2000() {
+        assert_eq!(rec_int_mult(1000, 2000), 2000000);
+    }
+
+    #[test]
+    fn test_rec_int_mult_large_int_x_1000_y_2001() {
+        assert_eq!(rec_int_mult(1000, 2001), 2001000);
+    }
 }
