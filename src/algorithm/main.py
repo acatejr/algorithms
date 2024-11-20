22c5f0bd94ecc2fbc20e6b690ac9a8fdd0fe18ba
acatejr <acatejr@gmail.com>
2024-11-20 16:21:57 -0700

    Finished rec_int_mult with a little help

--- src/algorithm/main.py
+++ src/algorithm/main.py
@@ -10,6 +10,9 @@ def rec_int_mult_str(x, y):
     200
     >>> rec_int_mult_str('2000', '1100')
     2200000
+
+    >>> rec_int_mult_str('5', '5')
+    25
     """
 
     if len(x) == 1 and len(y) == 1:
@@ -18,61 +21,13 @@ def rec_int_mult_str(x, y):
     split = len(x) // 2
     a, b = x[0:split], x[split:]
     c, d = y[0:split], y[split:]
-    # We cheat a bit here by multiplying by powers of 10 which can be implemented purely as addition
-    return (10 ** len(x)) * rec_int_mult_str(a, c) + \
-            (10 ** split) * (rec_int_mult_str(a, d) +\
-                             rec_int_mult_str(b, c)) + rec_int_mult_str(b, d)
-
-def rec_int_mult(x: int, y: int) -> int:
-    """Compute product of two terms recursively
-
-    Usage examples:
-    >>> rec_int_mult(0, 2)
-    0
-    >>> rec_int_mult(2, 0)
-    0
-    >>> rec_int_mult(1, 2)
-    2
-    >>> rec_int_mult(2, 1)
-    2
-    >>> rec_int_mult(10, 1)
-    10
-    >>> rec_int_mult(1, 10)
-    10
-    >>> rec_int_mult(10, 10)
-    100
-    >>> rec_int_mult(100, 100)
-    10000
-    """
-    prod = 0
-
-    if x == 0 or y == 0:
-        return x * y
-    if x == 1 or y == 1:
-        return x * y
-
-    n = max(len(str(x)), len(str(y)))
-    if n % 2 > 0:
-        
-        xpad = (n + 1) - len(str(x))
-        ypad = (n + 1) - len(str(y))
-        
-        x = "0" * xpad + str(x)
-        y = "0" * ypad + str(y)
-        a = int(x[0:len(x)//2])
-        b = int(x[len(x)//2:])
-        c = int(y[0:len(y)//2])
-        d = int(y[len(y)//2:])
-        ac = rec_int_mult(a, c)
-        ad = rec_int_mult(a, d)
-        bc = rec_int_mult(b, c)
-        bd = rec_int_mult(b, d)        
-
-        n = n + 1
-        prod = int((10**n * ac) + 10**(int(n/2)) * (ad + bc) + bd)        
-        return prod
-
     
+    # Shortcut multiplying by powers of 10, can be implemented purely as addition
+    return (10 ** len(x)) * rec_int_mult_str(a, c) + \
+            (10 ** split) * (rec_int_mult_str(a, d) + \
+                             rec_int_mult_str(b, c)) + rec_int_mult_str(b, d)    
+
+"""
 # if __name__ == "__main__":
     # x = 0
     # y = 20
@@ -133,3 +88,34 @@ def rec_int_mult(x: int, y: int) -> int:
 #     bd = rec_int_mult(b, d)
 #     print(ac, ad, bc, bd)
 #     prod = int((10**n_x * ac) + 10**(int(n_x/2)) * (ad + bc) + bd)
+"""
+
+"""
+def rec_int_mult(x: int, y: int) -> int:
+    prod = 0
+    if x == 0 or y == 0:
+        return x * y
+    if x == 1 or y == 1:
+        return x * y
+
+    n = max(len(str(x)), len(str(y)))
+    if n % 2 > 0:
+        
+        xpad = (n + 1) - len(str(x))
+        ypad = (n + 1) - len(str(y))
+        
+        x = "0" * xpad + str(x)
+        y = "0" * ypad + str(y)
+        a = int(x[0:len(x)//2])
+        b = int(x[len(x)//2:])
+        c = int(y[0:len(y)//2])
+        d = int(y[len(y)//2:])
+        ac = rec_int_mult(a, c)
+        ad = rec_int_mult(a, d)
+        bc = rec_int_mult(b, c)
+        bd = rec_int_mult(b, d)        
+
+        n = n + 1
+        prod = int((10**n * ac) + 10**(int(n/2)) * (ad + bc) + bd)        
+        return prod
+"""
